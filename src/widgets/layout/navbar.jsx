07090531f26797feaa4366e484a6751bdef249f5
Khaled Awartany <khaled.mo.mahmoud@outlook.com>
2025-12-18 11:35:41 +0300

    update from khaled

--- src/widgets/layout/navbar.jsx
+++ src/widgets/layout/navbar.jsx
@@ -83,7 +83,7 @@ export function Navbar({ brandName, routes, action }) {
 }
 
 Navbar.defaultProps = {
-  brandName: "Dot LinkMe",
+  brandName: "Dot LinkMe", 
   action: (
     <a
       href="https://www.creative-tim.com/product/material-tailwind-dashboard-react"
